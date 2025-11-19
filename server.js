5a9b03b079f4244660b85f448d8873891e9b4aa5
Facundo Castillo <facundocastillodev@gmail.com>
2025-11-18 21:25:15 -0300

    cambios para poder evaluar

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');      // <<----
 const { toRoman, fromRoman } = require('./index');
 const app = express();
 
+app.use(cors());                   // <<----
 app.use(express.json());
 
 // 🟩 Endpoint: arábigo → romano
